760fbdeb59c274e60ff5af18d2ada5e83ea22399
Вячеслав <ruzuk222@yandex.ru>
2023-11-21 10:38:42 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,117 +1,90 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
+п»ї#include <iostream>
+#include <math.h>
 
-double Summa(std::vector<double> Array)
+class Poligon
 {
-    double Sum = 0;
-    for (int i = 0; i < Array.size(); i++)
-    {
-        Sum += Array[i];
-    }
-    return Sum;
-}
-
-double ChangeLast(std::vector<double> Array)
-{
-    double Last;
-    double Sum = Summa(Array);
-    double Size = Array.size();
-    Last = (Sum / Size);
-    return Last;
-}
-
-void Show(std::vector<double> Array) // вывод вектора
-{
-    for (int i = 0; i < Array.size(); i++)
-    {
-        std::cout << std::fixed << std::setprecision(3) << Array[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
-void Change(std::vector<double> Array)
-{
-    double Sum = Summa(Array);
-    double Last = ChangeLast(Array);
-    Array.push_back(Sum);
-    Array.push_back(Last);
-    double SumLast = Summa(Array);
-    Show(Array);
-    double Del;
-    double Mnoj;
-    for (int i = 0; i < Array.size(); i++)
-    {
-        Array[i] -= SumLast;
-    }
-    Show(Array);
-    int operation = 10;
-    double element;
-    while (operation)
-    {
-        std::cin >> operation;
-        if (operation == 0)
-            break;
-        if (operation == 1)
-        {
-            std::cin >> element;
-            std::cout << "+: " << Array.size() + 1 << std::endl;
-            Array.push_back(element);
-            Show(Array);
-            double Sum = Summa(Array);
-            double Last = ChangeLast(Array);
-            Array.push_back(Sum);
-            Array.push_back(Last);
-            Show(Array);
-
-            double SumLast = Summa(Array);
-
-            for (int i = 0; i < Array.size(); i++)
-            {
-                Array[i] -= SumLast;
-            }
-            Show(Array);
-        }
-
-        if (operation == 2)
-        {
-            std::cout << "-: " << Array.size() - 1 << std::endl;
-            Array.pop_back();
-            Show(Array);
-            double Sum = Summa(Array);
-            double Last = ChangeLast(Array);
-            Array.push_back(Sum);
-            Array.push_back(Last);
-            Show(Array);
-
-            double SumLast = Summa(Array);
-
-            for (int i = 0; i < Array.size(); i++)
-            {
-                Array[i] -= SumLast;
-            }
-            Show(Array);
-        }
-    }
-}
+protected:
+	int* x = 0;
+	int* y = 0;
+	int count = 0;
+	int size;
+public:
+	Poligon()
+	{
+		std::cin >> size;
+		x = new int[size];
+		y = new int[size];
+		for (int i = 0; i < size; i++)
+			std::cin >> x[i];
+		for (int i = 0; i < size; i++)
+			std::cin >> y[i];
+	}
+	double calculateS()
+	{
+		double polygon = 0;
+		for (int i = 0; i < size - 1; i++)
+			polygon += ((x[i] - x[i + 1]) * (y[i] + y[i + 1]))/2.0;
+		polygon += ((x[size - 1] - x[0]) * (y[size - 1] + y[0])) / 2.0;
+		polygon = abs(polygon);
+		return polygon;
+	}
+	float calculateP()
+	{
+		double perimetr = 0;
+		for (int i = 0; i < size - 1; i++)
+			perimetr += sqrt(pow((x[i + 1] - x[i]), 2) + pow((y[i + 1] - y[i]), 2));
+		perimetr += sqrt(pow((x[size - 1] - x[0]), 2) + pow((y[size - 1] - y[0]), 2));
+		return perimetr;
+	}
+	friend bool operator^(Poligon p1, Poligon p2)
+	{
+		bool A = false, B = false, C = false, D = false;
+		bool intersection = false;
+		for (int i = 0; i < p1.size; i++)
+		{
+			for (int j = 0; j < p2.size; j++)
+			{
+				if (p1.x[i] < p2.x[j])
+					A = true;
+				if (p1.x[i] > p2.x[j])
+					B = true;
+				if (p1.y[i] > p2.y[j])
+					C = true;
+				if (p1.y[i] < p2.y[j])
+					D = true;
+			}
+			if (A && B && D && C)
+				intersection = true;
+			A = false;
+			B = false;
+			C = false;
+			D = false;
+		}
+		return intersection;
+	}
+	void console()
+	{
+		std::cout << "abscissa ";
+		for (int i = 0; i < size; i++)
+			std::cout << x[i] << " ";
+		std::cout << " ordinata ";
+		for (int i = 0; i < size; i++)
+			std::cout << y[i] << " ";
+		std::cout << " Perimetr= " << calculateP() << " Ploshad= " << calculateS() << std::endl;
+	}
+};
 
 int main()
 {
-    int n;
-    std::vector<double> Array;
-    std::cin >> n; // длина массива
-    double* array = new double[n];
-    for (int i = 0; i < n; i++) // заполнение массива
-    {
-        std::cin >> array[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        Array.push_back(array[i]);
-    }
-    std::cout << n << std::endl;
-    Show(Array); // вывод
-    Change(Array);
-    delete[] array;
-    return 0;
+	Poligon p1, p2;
+	std::cout << "Poligon1= ";
+	p1.console();
+	std::cout << "Poligon2= ";
+	p2.console();
+	/*bool fol = p1 ^ p2;*/
+	if (p1 ^ p2)
+		std::cout << "Poligon1 peresekaet Poligon2" << std::endl;
+	else
+		std::cout << "Poligon1 ne peresekaet Poligon2" << std::endl;
+	return 0;
 }
